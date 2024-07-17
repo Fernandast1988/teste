9ca0f74d4e98e23962610b822832767332c8f23e
Programação em Python 2024.4 <36129532024.4@senacrjedu.local>
2024-07-17 12:02:40 -0300

    Testando

---
+++ if.py
@@ -0,0 +1,26 @@
+# verificar se é maior idade
+
+
+maiorIdade = 18
+idade = int(input('digite sua idade\n'))
+
+
+if (idade >= maiorIdade):
+    a=1
+    print('eh maior de idade')
+else:
+    print('eh menor de idade')
+
+#if(variavel'sinal'variavel)
+# é igual/diferente?
+#igual:==
+#diferente:!=
+# é maior/menor?
+# Maior:>(Maior ou igual:>=)
+# Menor:<(Menor ou igual:<=)
+
+
+
+
+
+
